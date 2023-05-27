d1c35a5380d7bdf4f45128616091a2e5d015f1d4
Tano <gerardo.martinez.carmona@gmail.com>
2023-05-26 21:29:47 -0500

    jj

--- proyectofinal/java/menu.js
+++ proyectofinal/java/menu.js
@@ -1,6 +1,31 @@
+
 let btnMenu = document.getElementById('btnmenu');
 let menu = document.getElementById('menu');
 btnMenu.addEventListener('click', function(){
    'use strict';
     menu.classList.toggle('mostrar');
+});
+
+
+
+window.addEventListener('load',async function(){
+    let items = await fetch('/items.json');
+    items = await items.json();
+    const shop = document.getElementById('shop');
+    console.log(items);
+    items.forEach(item => {
+    shop.innerHTML += `
+    <div class="item">
+        <figure>
+            <img src="${item.img}" alt="camisaAAA" />
+        </figure>
+        <div class="info-product">
+            <h2>${item.nombre} </h2>
+            <p class="price">${item.precio} </p>
+            <button>Añadir al carrito</button>
+        </div>
+    </div>
+    `;
+    });
+
 });
